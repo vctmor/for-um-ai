7f7df0834360cc48cd1f0da40c9ee52fcf9e5194
victor_moreira <victormoreira@vivaldi.net>
2024-06-13 20:22:09 -0300

    Adiciona notações @Valid na DataTopic

---
+++ src/main/java/com/amoreira/for_um_ai/topic/DataTopic.java
@@ -0,0 +1,11 @@
+package com.amoreira.for_um_ai.topic;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DataTopic(@Valid
+        @NotBlank String title,
+        @NotBlank String message,
+        @NotBlank String course
+) { }
