91974bc4f605d36ec2786c5d9abfcb22d03b62d8
victor_moreira <victormoreira@vivaldi.net>
2024-06-20 08:10:00 -0300

    testagem manual da autentificação e controle de acesso da API

--- src/main/java/com/amoreira/for_um_ai/infra/security/SecurityConfigurations.java
+++ src/main/java/com/amoreira/for_um_ai/infra/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package com.amoreira.for_um_ai.infra.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,20 +12,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, @Autowired SecurityFilter securityFilter) throws Exception {
 
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll();
                     req.anyRequest().authenticated();
-                }).build();
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
     }
 
     @Bean
